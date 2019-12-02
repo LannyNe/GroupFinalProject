c156dc6760155c9e8e443e2e24e424a9da10619f
Quinten Ross <qjross76@gmail.com>
2019-12-01 19:00:59 -0500

    got some of the jquery done; the mouseleave is not gonna stick, though

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-Placehold
+var n = 0
+
+$(document).ready(function() {
+
+$(".nav-link").mouseenter(function(){
+ $(this).css({"background-color": "#204593", color: "white"});
+ });
+    
+$(".nav-link").mouseleave(function(){
+    $(this).css("background-color", "gray");
+    
+});
+    });
